e823c46b68f72640cf63da266da621cd38da017e
Joe <74819346+JoeBrunton@users.noreply.github.com>
2026-02-20 13:06:13 +0000

    0.2.0
    Added in a LOGO asset set with accompanying LOGOs.swift file exposing the logos as Image objects.
    Created a ReuseButtonView

---
+++ Sources/OnlyCoreKit/UI/Components/ReuseButtonView.swift
@@ -0,0 +1,67 @@
+//
+//  ReuseButtonView.swift
+//  OnlyCoreKit
+//
+//  Created by Joe Brunton on 20/02/2026.
+//
+
+import SwiftUI
+
+public struct ReuseButtonView: View {
+    
+    private var text: String = "Push Me"
+    private var image: Image? = nil
+    private var isCentre: Bool = true
+    private var foreColour: Color = .white
+    private var backColour: Color = OnlyAppColours.onlyPaletteSuccess
+    private var width: CGFloat = CGFloat.infinity
+    private var height: CGFloat = 60
+    private var padding: CGFloat = 18
+    
+    public init(text: String,
+                image: Image? = nil,
+                isCentre: Bool = true,
+                foreColour: Color = Color.white,
+                backColour: Color = OnlyAppColours.onlyPaletteSuccess,
+                width: CGFloat = CGFloat.infinity,
+                height: CGFloat = 60,
+                padding: CGFloat = 18) {
+        self.text = text
+        self.image = image
+        self.isCentre = isCentre
+        self.foreColour = foreColour
+        self.backColour = backColour
+        self.width = width
+        self.height = height
+        self.padding = padding
+    }
+    
+    
+    public var body: some View {
+        HStack {
+            if let image = image {
+                image
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .padding(.leading, isCentre ? 18 : 25)
+                    .padding(.trailing, 8)
+            }
+            
+            Text(text)
+                .padding(image != nil ? [.vertical, .trailing] : .all)
+                .foregroundStyle(.white)
+            
+            if !isCentre {
+                Spacer()
+            }
+        }
+        .frame(maxWidth: width, maxHeight: height)
+        .background(backColour)
+        .clipShape(.buttonBorder)
+        .padding(padding)
+    }
+}
+
+#Preview {
+    ReuseButtonView(text: "Push Me", image: LOGOs.onlyDefaultLogo, isCentre: false)
+}
---
+++ Sources/OnlyCoreKit/UI/LOGOs.swift
@@ -0,0 +1,22 @@
+//
+//  LOGOs.swift
+//  OnlyCoreKit
+//
+//  Created by Joe Brunton on 20/02/2026.
+//
+
+import Foundation
+import SwiftUI
+
+/// exposure
+public enum LOGOs {
+    
+    public static let onlyDefaultLogo = Image("OnlyLogoDefault", bundle: .module)
+    public static let appleLight = Image("Apple_logo_grey", bundle: .module)
+    public static let appleDark = Image("Apple_logo_black", bundle: .module)
+    public static let google = Image("Google", bundle: .module)
+    public static let instagram = Image("instagram", bundle: .module)
+    public static let snapchat = Image("snapchat", bundle: .module)
+    public static let spotify = Image("spotify", bundle: .module)
+    public static let whatsapp = Image("whatsapp", bundle: .module)
+}
